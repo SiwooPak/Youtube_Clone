43c2fffac2839e0b3b949aba0b7a892d09f24782
SiwooPak <bogus1620@naver.com>
2021-06-16 04:18:24 +0900

    implemented: SideVideo

---
+++ client/src/components/views/VideoDetailPage/Sections/SideVideo.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react'
+import Axios from 'axios';
+function SideVideo() {
+
+    const [SideVideos, setSideVideos] = useState([]);
+
+    useEffect(() => {
+        Axios.get("/api/video/getVideos").then((response) => {
+          if (response.data.success) {
+            //console.log(response.data);
+            setSideVideos(response.data.videos);
+          } else {
+            alert("video null.");
+          }
+        });
+      }, []);
+    
+    const renderSideVideo = SideVideos.map((video, index) => {
+        let min = Math.floor(video.duration / 60);
+        let sec = Math.floor(video.duration - min * 60);
+
+        return <div key={index} style={{ display: 'flex', marginBottom: '1rem', padding: '0 2rem'}}>
+        <div style={{ width: '70%', marginRight: '1rem'}}>
+            <a href>
+                <img style={{width: '100%', height: '100%'}} 
+                    src={`http://localhost:5000/${video.thumbnail}`} alt={video.title} />
+            </a>
+        </div>
+
+        <div style={{width: '50%'}}>
+            <a href style={{ color: 'gray'}}>
+                <span style={{ fontSize: '1rem', color: 'black'}}>{video.title}</span><br />
+                <span>{video.writer.name}</span> <br />
+                <span>{video.views}views</span> <br />
+                <span>{min}:{sec}</span> <br />
+            </a>
+        </div>
+        
+    </div>
+    }) 
+        
+  
+
+    return (
+        <React.Fragment>
+            {renderSideVideo}
+        </React.Fragment>
+        
+    )
+}
+
+export default SideVideo
