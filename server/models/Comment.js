75495ff7f7057c5708e389bc0fade8c1b3bc6c4b
SiwooPak <bogus1620@naver.com>
2021-06-21 05:33:13 +0900

    added: comment model, component added

---
+++ server/models/Comment.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const saltRounds = 10;
+
+const moment = require("moment");
+
+const subscriberSchema = mongoose.Schema({
+    writer: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    postId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Video'
+    },
+    responseTo: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    content: {
+        type: String
+    }
+}, {timestamps: true})
+
+
+
+
+const Comment = mongoose.model('Comment', commentSchema);
+
+module.exports = { Comment }
